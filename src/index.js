a19fbe69c88e3c16cc444aa151c919684c740f50
munkoev <munkoev.e@yandex.ru>
2021-03-04 19:17:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length>0 ) {
+      let sorted = array.sort((a,b) => {
+          return a-b;
+      })
+      return sorted[0]
+  } else {
+      return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array !== 'undefined' && array.length>0) {
+        let sorted = array.sort((a,b) => {
+            return a-b;
+        })
+        return sorted[sorted.length-1]
+    } else {
+        return +0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array !== 'undefined' && array.length>0) {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return array.reduce(reducer,0) / array.length;
+    } else {
+        return 0;
+    }
 }
